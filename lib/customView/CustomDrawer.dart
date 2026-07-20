5106c71464e6d1f671589d820c4a4dcaed1fbef1
NabuzTech2025 <nabuztech@gmail.com>
2026-07-20 12:34:40 +0530

    Ios TestFlight

--- lib/customView/CustomDrawer.dart
+++ lib/customView/CustomDrawer.dart
@@ -335,7 +335,7 @@ class _CustomDrawerState extends State<CustomDrawer> {
                   ),
                 Padding(
                   padding: const EdgeInsets.only(left: 15.0),
-                  child: Text('${'version'.tr}: 2.2(3)', style: const TextStyle(
+                  child: Text('${'version'.tr}: 2.3(1)', style: const TextStyle(
                       fontWeight: FontWeight.w300,
                       fontSize: 15
                   ),),
